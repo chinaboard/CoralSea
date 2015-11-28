35ba15efbbaa45d0561409b5f66bad44c2ff694a
zhenyfeng <zhenyfeng@163.com>
2015-11-28 21:45:02 +0800

    修正项目类型创建错误的问题

--- CoralSea/CoralSea.IBusiness/ISecurityBusinesscs.cs
+++ CoralSea/CoralSea.IBusiness/ISecurityBusinesscs.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace CoralSea.IDataAccess
+namespace CoralSea.IBusiness
 {
-    public class Class1
+    public interface ISecurityBusinesscs
     {
+        
     }
 }
---
+++ CoralSea/CoralSea.Model/UserVerifyResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoralSea.Model
+{
+    public class UserVerifyResult
+    {
+        public bool Result { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
