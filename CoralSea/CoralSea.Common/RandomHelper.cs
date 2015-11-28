28af7caaef2d03f382e4b223b101ef45ff79bce8
chinaboard <chinaboard@gmail.com>
2015-11-29 03:45:04 +0800

    fakeuser

---
+++ CoralSea/CoralSea.Common/RandomHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoralSea.Common
+{
+    public static class RandomHelper
+    {
+        static Random _random = new Random();
+
+        public static bool Bool()
+        {
+            return _random.Next(100) > 50;
+        }
+
+        public static int Next()
+        {
+            return _random.Next();
+        }
+        public static int Next(int maxValue)
+        {
+            return _random.Next(maxValue);
+        }
+        public static int Next(int minValue, int maxValue)
+        {
+            return _random.Next(minValue, maxValue);
+        }
+    }
+}
