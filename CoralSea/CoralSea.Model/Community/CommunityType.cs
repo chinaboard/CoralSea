da8dd4dbc7bb81e68a88cf39e164a434b6637aa1
zhenyfeng <zhenyfeng@163.com>
2015-11-29 02:43:30 +0800

    增加用户信息Model以及业务逻辑接口

---
+++ CoralSea/CoralSea.Model/Community/CommunityType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoralSea.Model.Community
+{
+    /// <summary>
+    /// 圈子的类型
+    /// </summary>
+    public enum CommunityType : int
+    {
+        /// <summary>
+        /// 组织架构
+        /// </summary>
+        [Description("组织架构")]
+        RealGroup = 0x01,
+
+        /// <summary>
+        /// 专项
+        /// </summary>
+        [Description("专项")]
+        VirtualGroup = 0x01 << 1,
+
+        /// <summary>
+        /// 兴趣
+        /// </summary>
+        [Description("兴趣")]
+        InterestingGroup = 0x01 << 2
+    }
+}
