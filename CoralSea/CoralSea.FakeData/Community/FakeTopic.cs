75ef1b8f5f480d10e7831c74876dd626afb5c206
chinaboard <chinaboard@gmail.com>
2015-11-29 04:57:01 +0800

    fake topic

---
+++ CoralSea/CoralSea.FakeData/Community/FakeTopic.cs
@@ -0,0 +1,24 @@
+﻿using CoralSea.Common;
+using CoralSea.Model.Community;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoralSea.FakeData.Community
+{
+    public class FakeTopic : TopicModel
+    {
+        public FakeTopic(int topicId)
+        {
+            TopicId = topicId;
+            CreatorId = RandomHelper.Next(10000);
+            TopicTitle = FakeName.MakeFakeName() + "发了个帖子";
+            for (int i = 0; i < RandomHelper.Next(2, 10); i++)
+                ReplyList.Add(new FakeTopicReply(topicId, RandomHelper.Next(10000)));
+            CreateTime = RandomHelper.Time();
+            LastUpdateTime = RandomHelper.Time();
+        }
+    }
+}
