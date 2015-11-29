d53dfe9db341e21072cf5b512638b9558f46b5d9
chinaboard <chinaboard@gmail.com>
2015-11-29 09:44:41 +0800

    fake tag

---
+++ CoralSea/CoralSea.FakeData/Tag/FakeTag.cs
@@ -0,0 +1,20 @@
+﻿using CoralSea.Common;
+using CoralSea.Model.Tag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoralSea.FakeData.Tag
+{
+    public class FakeTag : TagModel
+    {
+        public FakeTag()
+        {
+            TagId = RandomHelper.Next(35345);
+            TagName = FakeName.MakeFakeName();
+            HotDegree = RandomHelper.Next(5000);
+        }
+    }
+}
